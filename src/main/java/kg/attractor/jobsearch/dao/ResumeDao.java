6b5f302fb882eb7dff0d037d5b7831bdc17c640b
kutush <kutmanokenov18@gmail.com>
2025-03-25 17:44:15 +0300

    adding methods for resume

--- src/main/java/kg/attractor/jobsearch/dao/ResumeDao.java
+++ src/main/java/kg/attractor/jobsearch/dao/ResumeDao.java
@@ -7,6 +7,10 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 @Component
@@ -29,4 +33,37 @@ public class ResumeDao {
         Resumes resumes = jdbcTemplate.queryForObject(sql,new BeanPropertyRowMapper<>(Resumes.class),applicant_id);
         return Optional.ofNullable(resumes);
     }
+     public Optional<Resumes> getResumeById(int  resume_id){
+       String sql = "select * from resumes where id like ? LIMIT 1";
+       Resumes resumes = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Resumes.class),resume_id);
+       return Optional.ofNullable(resumes);
+    }
+    public void updateResume(Resumes resumes) {
+        String sql = "UPDATE resumes SET name = ?, category_id = ?, salary = ?, is_active = ?, applicant_id = ? WHERE id = ?;\n";
+        jdbcTemplate.update(sql,
+                resumes.getName(),
+                resumes.getCategoryId(),
+                resumes.getSalary(),
+                resumes.isIs_active(),
+                resumes.getApplicant_id(),
+                resumes.getId());
+    }
+
+    public void save(Resumes resume) {
+        String sql = "INSERT INTO resumes (name, category_id, salary, is_active, created_date, update_time, applicant_id) " + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        jdbcTemplate.update(sql,
+                resume.getName(),
+                resume.getCategoryId(),
+                resume.getSalary(),
+                resume.isIs_active(),
+                resume.getCreated_date(),
+                resume.getUpdate_time(),
+                resume.getApplicant_id());
+    }
+
+
+
+
+
 }
--- src/main/java/kg/attractor/jobsearch/dto/ResumeDto.java
+++ src/main/java/kg/attractor/jobsearch/dto/ResumeDto.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 @Data
 @Builder
 public class ResumeDto {
