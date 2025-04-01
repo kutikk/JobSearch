9d7a35e1eaf46d4a33fe1ff44081e2ae15dad8b6
kutush <kutmanokenov18@gmail.com>
2025-04-01 17:32:10 +0300

    add table authorities

--- src/main/resources/db/changelog/data.sql
+++ src/main/resources/db/changelog/data.sql
@@ -36,3 +36,34 @@ ALTER TABLE WORK_EXPERIENCE_INFO
         FOREIGN KEY (resume_id)
             REFERENCES resumes(id)
             ON DELETE CASCADE;
+
+
+INSERT INTO users (user_name, email, password, phone_number, avatar, age, account_type,ENABLED)
+VALUES
+    ('admin', 'admin@example.com', 'employerpassword', '1234567890', 'employer_avatar.jpg', 35, 'employer',true),
+    ('user', 'user@example.com', 'applicantpassword', '0987654321', 'applicant_avatar.jpg', 28, 'applicant',true);
+
+
+INSERT INTO authorities (authority, user_id)
+VALUES
+    ('ADMIN', 7),
+    ('USER', 8);
+
+INSERT INTO users (user_name, email, password, phone_number, avatar, age, account_type,ENABLED)
+VALUES
+    ('adminkutman', 'admin@example.com', '$2a$12$lUsg9FvT6MmPcEUg6DLmxu2YJbyxjlCEo3tVjl2IWn6d2foVCNOc2', '1234567890', 'employer_avatar.jpg', 35, 'employer',true),
+    ('userkuti', 'user@example.com', '$2a$12$ATqf6K89OuwyA9lsEDucpe19cSQ6w.q2Tsu5V0fO9df.M4jUGehXK', '0987654321', 'applicant_avatar.jpg', 28, 'applicant',true);
+
+INSERT INTO authorities (authority, user_id)
+VALUES
+    ('ROLE_ADMIN', 10),
+    ('ROLE_USER', 11);
+
+SELECT user_name, password, enabled FROM users WHERE user_name = 'adminkutman';
+SELECT u.user_name, a.authority
+FROM users u
+         JOIN authorities a ON u.ID = a.user_id
+WHERE u.user_name = 'adminkutman';
+
+DELETE FROM authorities
+WHERE user_id IN (7, 8, 10, 11);
